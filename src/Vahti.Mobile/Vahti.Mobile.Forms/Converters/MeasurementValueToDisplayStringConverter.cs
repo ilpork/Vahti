ed9a349199f81bdd10390b01eb93f7f85b51a84a
Ilpo R-K <53601355+ilpork@users.noreply.github.com>
2020-04-27 18:57:21 +0300

    Add a common class to produce display values (#7)
    It makes XAML lighter and allows using same functionality from widget

--- src/Vahti.Mobile/Vahti.Mobile.Forms/Converters/MeasurementValueToDisplayStringConverter.cs
+++ src/Vahti.Mobile/Vahti.Mobile.Forms/Converters/MeasurementValueToDisplayStringConverter.cs
@@ -1,17 +1,22 @@
 ﻿using System;
-using System.Globalization;
+using Vahti.Mobile.Forms.Localization;
+using Vahti.Mobile.Forms.Models;
 using Xamarin.Forms;
 
 namespace Vahti.Mobile.Forms.Converters
 {
     /// <summary>
-    /// Converts string to double
+    /// Converts <see cref="Measurement"/> to string suitable for showing in UI
     /// </summary>
-    public class StringToDoubleConverter : IValueConverter
+    public class MeasurementToDisplayStringConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            return value == null ? 0 : double.Parse(value.ToString(), CultureInfo.InvariantCulture);            
+            if (value != null && value is Measurement)
+            {
+                return DisplayValueFormatter.GetMeasurementDisplayValue((Measurement)value);
+            }
+            return null;
         }
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
