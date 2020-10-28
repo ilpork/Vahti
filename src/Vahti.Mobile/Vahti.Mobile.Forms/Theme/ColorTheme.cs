761dd52187ed0e9a3a08bfb8eba8737dba6f86be
Ilpo R-K <53601355+ilpork@users.noreply.github.com>
2020-10-28 20:00:12 +0200

    Define color separately for Switch thumb
    After XF update the white thumb color does not have shadow, and it's not visible against white background

--- src/Vahti.Mobile/Vahti.Mobile.Forms/Theme/ColorTheme.cs
+++ src/Vahti.Mobile/Vahti.Mobile.Forms/Theme/ColorTheme.cs
@@ -29,6 +29,7 @@ namespace Vahti.Mobile.Forms.Theme
             Application.Current.Resources["ThemeOnSurface"] = Application.Current.Resources[$"{colorTheme}ThemeOnSurface"];
             Application.Current.Resources["ThemeUnselectedTabText"] = Application.Current.Resources[$"{colorTheme}ThemeUnselectedTabText"];
             Application.Current.Resources["ThemeEntryBackground"] = Application.Current.Resources[$"{colorTheme}ThemeEntryBackground"];
+            Application.Current.Resources["ThemeSwitchThumbColor"] = Application.Current.Resources[$"{colorTheme}ThemeSwitchThumbColor"];
 
             _themeChanger.ApplyTheme(colorTheme);
             
