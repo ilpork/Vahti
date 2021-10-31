d080a8f2d3812c92a60168f8492e7c065f8b45a2
Ilpo R-K <53601355+ilpork@users.noreply.github.com>
2021-10-31 20:22:04 +0200

    Fix start-up problem occurring if app.config does not used/exist
    Start-up crashed due to route to options page did not exist

--- src/Vahti.Mobile/Vahti.Mobile.Forms/AppShell.xaml.cs
+++ src/Vahti.Mobile/Vahti.Mobile.Forms/AppShell.xaml.cs
@@ -10,7 +10,8 @@ namespace Vahti.Mobile.Forms
         public AppShell()
         {
             InitializeComponent();
-                        
+
+            Routing.RegisterRoute("options", typeof(OptionsGeneralPage));
             Routing.RegisterRoute("location", typeof(LocationGraphPage));
             Routing.RegisterRoute("location/details", typeof(LocationDetailsPage));
         }
