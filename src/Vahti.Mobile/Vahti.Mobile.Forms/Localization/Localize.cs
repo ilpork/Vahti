038c53cb3e79cc7249371ca6897eaa52e954a6a0
Ilpo R-K <53601355+ilpork@users.noreply.github.com>
2023-07-30 10:03:13 +0300

    Migrate iOS related changes to MAUI project

--- src/Vahti.Mobile/Vahti.Mobile.Forms/Localization/Localize.cs
+++ src/Vahti.Mobile/Vahti.Mobile.Forms/Localization/Localize.cs
@@ -1,4 +1,7 @@
 ﻿using System.Globalization;
+#if IOS
+using Foundation;
+#endif
 
 namespace Vahti.Mobile.Forms.Localization
 {    
@@ -12,6 +15,10 @@ namespace Vahti.Mobile.Forms.Localization
         {
 #if ANDROID
             Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture = ci;
+#endif
+#if IOS
+            Thread.CurrentThread.CurrentCulture = ci;
+            Thread.CurrentThread.CurrentUICulture = ci;
 #endif
         }
 
@@ -39,6 +46,35 @@ namespace Vahti.Mobile.Forms.Localization
                     ci = new CultureInfo("en");
                 }
             }
+#endif
+#if IOS
+            var netLanguage = "en";
+            if (NSLocale.PreferredLanguages.Length > 0)
+            {
+                var pref = NSLocale.PreferredLanguages[0];
+                netLanguage = iOSToDotnetLanguage(pref);
+            }
+            // this gets called a lot - try/catch can be expensive so consider caching or something
+            
+            try
+            {
+                ci = new System.Globalization.CultureInfo(netLanguage);
+            }
+            catch (CultureNotFoundException)
+            {
+                // iOS locale not valid .NET culture (eg. "en-ES" : English in Spain)
+                // fallback to first characters, in this case "en"
+                try
+                {
+                    var fallback = ToDotnetFallbackLanguage(new PlatformCulture(netLanguage));
+                    ci = new System.Globalization.CultureInfo(fallback);
+                }
+                catch (CultureNotFoundException)
+                {
+                    // iOS language not valid .NET culture, falling back to English
+                    ci = new System.Globalization.CultureInfo("en");
+                }
+            }            
 #endif
             return ci;
         }
@@ -68,7 +104,7 @@ namespace Vahti.Mobile.Forms.Localization
         private string ToDotnetFallbackLanguage(PlatformCulture platCulture)
         {
             var netLanguage = platCulture.LanguageCode; // use the first part of the identifier (two chars, usually);
-#if ANDROID            
+#if ANDROID
             switch (platCulture.LanguageCode)
             {
                 case "gsw":
@@ -76,7 +112,43 @@ namespace Vahti.Mobile.Forms.Localization
                     break;
             }
 #endif
+#if IOS            
+            switch (platCulture.LanguageCode)
+            {
+                case "pt":
+                    netLanguage = "pt-PT"; // fallback to Portuguese (Portugal)
+                    break;
+                case "gsw":
+                    netLanguage = "de-CH"; // equivalent to German (Switzerland) for this app
+                    break;
+                    // add more application-specific cases here (if required)
+                    // ONLY use cultures that have been tested and known to work
+            }            
+#endif
             return netLanguage;
         }
+
+#if IOS
+        string iOSToDotnetLanguage(string iOSLanguage)
+        {
+            // .NET cultures don't support underscores
+            string netLanguage = iOSLanguage.Replace("_", "-");
+
+            //certain languages need to be converted to CultureInfo equivalent
+            switch (iOSLanguage)
+            {
+                case "ms-MY":   // "Malaysian (Malaysia)" not supported .NET culture
+                case "ms-SG":    // "Malaysian (Singapore)" not supported .NET culture
+                    netLanguage = "ms"; // closest supported
+                    break;
+                case "gsw-CH":  // "Schwiizertüütsch (Swiss German)" not supported .NET culture
+                    netLanguage = "de-CH"; // closest supported
+                    break;
+                    // add more application-specific cases here (if required)
+                    // ONLY use cultures that have been tested and known to work
+            }
+            return netLanguage;
+        }
+#endif
     }
 }
