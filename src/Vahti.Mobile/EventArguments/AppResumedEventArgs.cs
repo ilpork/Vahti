4c630f44d47414f65b95112b1aa8024c9109169a
Ilpo R-K <53601355+ilpork@users.noreply.github.com>
2023-08-01 08:47:36 +0300

    Fix issues related to location and graph page not getting updated when app is resumed
    Behavior of Page.Appearing has changed in MAUI, so it's called only once for the page. As workaround a new static event is created for the App to indicate when the app has been resumed, so the page can refresh if needed.

---
+++ src/Vahti.Mobile/EventArguments/AppResumedEventArgs.cs
@@ -0,0 +1,15 @@
+﻿namespace Vahti.Mobile.EventArguments
+{
+    /// <summary>
+    /// Event arguments used when app is resumed
+    /// </summary>
+    public class AppResumedEventArgs : EventArgs
+    {
+        public Page CurrentPage { get; }       
+
+        public AppResumedEventArgs(Page currentPage)
+        {
+            CurrentPage = currentPage;
+        }
+    }
+}
