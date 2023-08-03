51f393c65996eaa8107bc0e8f6d8da2a36e2e727
Ilpo R-K <53601355+ilpork@users.noreply.github.com>
2023-08-03 14:34:53 +0300

    Tick version to v2.0.0
    Mobile app has been updated (and restructured) with some breaking changes.

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © 2022")]
 [assembly: AssemblyTrademark("")]
 
-[assembly: AssemblyVersion("1.2.3")]
-[assembly: AssemblyFileVersion("1.2.3")]
+[assembly: AssemblyVersion("2.0.0")]
+[assembly: AssemblyFileVersion("2.0.0")]
